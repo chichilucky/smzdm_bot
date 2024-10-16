e8aa1468f1a9b4c33b7dcf5e57efc2484ad728f6
chichilucky <82249389+chichilucky@users.noreply.github.com>
2024-10-16 09:58:29 +0800

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import os
 import sys
 from pathlib import Path
-
 from loguru import logger
 from notify.notify import NotifyBot
 from utils.file_helper import TomlHelper
 from utils.smzdm_bot import SmzdmBot
 from utils.smzdm_tasks import SmzdmTasks
+from utils.hlh_checkin import hlh
 
 CURRENT_PATH = Path(__file__).parent.resolve()
 CONFIG_FILE = Path(CURRENT_PATH, "config/config.toml")
@@ -31,13 +31,25 @@ def load_conf():
             "TG_BOT_TOKEN": os.environ.get("TG_BOT_TOKEN", None),
             "TG_USER_ID": os.environ.get("TG_USER_ID", None),
             "TG_BOT_API": os.environ.get("TG_BOT_API", None),
+            "OPENIDS": os.environ.get("HLH_OPENID"),
         }
+        st=str(os.environ.get("HLH_OPENID"))
+        logger.info(f"Get {st}")
         conf_kwargs.update({"env_conf": True})
     else:
         logger.info("Please set cookies first")
         sys.exit(1)
     return conf_kwargs
 
+def hlh_checkin_all(openids: str) -> str:
+    import json
+    opids=json.loads(openids)
+    msg = ''
+    for k, v in opids.items():
+        msg+=f"==========用户{k}===========\n"
+        hl = hlh(OPENID=v)
+        msg+=hl.checkin()
+    return msg
 
 def main():
     conf_kwargs = load_conf()
@@ -56,6 +68,8 @@ def main():
                 msg += tasks.all_reward()
                 tasks.extra_reward()
                 msg += tasks.lottery()
+                hl=hlh(**config)
+                msg += hl.checkin()
             except Exception as e:
                 logger.error(e)
                 continue
@@ -71,11 +85,14 @@ def main():
         msg += tasks.all_reward()
         tasks.extra_reward()
         msg += tasks.lottery()
+        logger.info(f"Start hlh check in ")
+        msg += hlh_checkin_all(conf_kwargs["OPENIDS"])
+        oids=conf_kwargs["OPENIDS"]
+        logger.info(f" hlh check {oids} ")
         NotifyBot(content=msg, **conf_kwargs)
     if msg is None or "Fail to login in" in msg:
         logger.error("Fail the Github action job")
         sys.exit(1)
-
 
 if __name__ == "__main__":
     main()
